5445bcee0db1d62293c62bf8671a9d5fa752c932
kahnamuei80 <mj.kahnamuei@gmail.com>
2025-05-03 21:54:18 +0330

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  console.log('test');
+
+  const renderName = (name2: any) => {
+    const name = 'mohammad';
+    return <span>{name}</span>;
+  };
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div>my first Page</div>
+      {renderName(1)}
+    </div>
+  );
+}
